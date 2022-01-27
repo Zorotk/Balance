5b8ff7387dca9a186bc9cd59498c634e9ab0cc3a
Pavel <zorotkov@gmail.com>
2022-01-27 20:09:01 +0300

    add button

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,4 +1,4 @@
-import { Button, Progress } from "antd";
+import { Button, Input, Progress } from "antd";
 import React, { useState } from "react";
 
 const Balance = () => {
@@ -11,10 +11,33 @@ const Balance = () => {
   };
   const [cripto, setcripto] = useState<Record<string, number>>({ ...data });
   const [maxCount, setmaxCount] = React.useState(1);
-  const arr = Object.entries(cripto).sort(([, a], [, b]) => b - a);
+  const [balanceCount, setbalanceCount] = React.useState(3);
+
+  let j = 0;
+  const memo = (fun: any) => {
+    let name = "";
+    let result: string;
+    return (args: string) => {
+      if (name !== args) {
+        result = fun(args);
+        name = args;
+      }
+      return result;
+    };
+  };
+
+  const hi = memo((name: string) => `${name} ${j++}`);
+  console.log(hi("alex")); // alex 0
+  console.log(hi("alex")); // alex 0
+  console.log(hi("alex")); // alex 0
+  console.log(hi("GLEB")); // GLEB 1
+  console.log(hi("GLEB")); // GLEB 1
+  console.log(hi("OLEG")); // OLEG 2
+
   let i = 0;
 
   function equalProportion(amount: number) {
+    const arr = Object.entries(cripto).sort(([, a], [, b]) => b - a);
     if (amount <= 0) return arr;
     while (amount !== 0) {
       setmaxCount(Math.max(arr[i][1]));
@@ -32,9 +55,11 @@ const Balance = () => {
     arr.forEach(([k, v]) => {
       cripto[k] = v;
     });
+
     setcripto({ ...cripto });
     return arr;
   }
+
   const percent = (v: number, maxCount: number) =>
     +((v / maxCount) * 100).toFixed();
 
@@ -42,14 +67,28 @@ const Balance = () => {
     setcripto({ ...data });
     setmaxCount(1);
   };
+  const operation = (k: string, v: string) => {
+    v === "+" ? (cripto[k] += 1) : (cripto[k] -= 1);
+    setcripto({ ...cripto });
+  };
 
   return (
     <div className={"balance"}>
-      <Button onClick={() => equalProportion(1)}>Balance</Button>
+      <Input
+        type="number"
+        value={balanceCount}
+        onChange={(e: any) => setbalanceCount(e.target.value)}
+      />
+      <Button onClick={() => equalProportion(balanceCount)}>Balance</Button>
       {Object.entries(cripto).map(([k, v]) => (
         <div className="cripto" key={k}>
-          {k} {v}
-          <Progress percent={percent(v, maxCount)} />
+          <div>
+            {k}
+            <Button onClick={() => operation(k, "+")}>+</Button>
+            {v}
+            <Button onClick={() => operation(k, "-")}>-</Button>
+            <Progress percent={percent(v, maxCount)} />
+          </div>
         </div>
       ))}
       <Button onClick={defaults}>Defaults</Button>
